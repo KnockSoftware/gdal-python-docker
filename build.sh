3e852334bf2a7d6c793a9975c97a8f06c71d2889
Robert Hopson <robert.hopson@gmail.com>
2019-07-25 17:30:08 -0700

    Use debian-packaged gdal. Workaround buster's tighter default SSL settings.

--- build.sh
+++ build.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-#tags="3.4.9-stretch 3.5.6-stretch 3.6.7-stretch 3.7.3-stretch 2.7.16-stretch"
-tags="3.7.3-stretch"
-#tags="2.7.16-stretch"
-#gdals="2.3.1 2.3.2"
-gdals="2.3.2"
+tags="3.7.4-buster"
+repo=518982846871.dkr.ecr.us-west-2.amazonaws.com/gdal-python
 
 set -ex
 
-for python_image_tag in $tags; do
-  for gdal_version in $gdals; do
-    TAG=$python_image_tag--gdal-$gdal_version
-    docker build --build-arg "GDAL_VERSION=$gdal_version" --build-arg "PYTHON_DOCKER_TAG=$python_image_tag" -t ridereport/gdal-python:$TAG .
-    docker push ridereport/gdal-python:$TAG
-  done
+for python_image_tag in "$tags"; do
+  tag=$python_image_tag-$(date +%Y%m%d)
+  docker build -t ${repo}:${tag} .
+  docker push ${repo}:${tag}
 done
 
 # tag most recent version as latest
-docker tag ridereport/gdal-python:$TAG ridereport/gdal-python:latest
-docker push ridereport/gdal-python:latest
+docker tag ${repo}:$tag ${repo}:latest
+docker push ${repo}:latest
